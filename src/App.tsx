121ed86cee652c4cc85cde521db135151c0f8149
jeanpierre <jean.gonzales1414@gmail.com>
2022-11-30 22:13:51 -0500

    nice styling YEP

--- src/App.tsx
+++ src/App.tsx
@@ -57,8 +57,6 @@ function App() {
         background: "#010203",
         borderRadius: "1%",
         padding: "0.5rem",
-        boxSizing: "border-box",
-        overflowX: "hidden",
       }}
     >
       <div style={{ fontSize: "2rem", textAlign: "center", color: "white" }}>
